b00d0c1a7f780941e95a6aea77ab235d9d9cd327
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2026-01-22 15:10:21 +0800

    deps: install preact inside astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ukesjtu.github.io",
-    base: import.meta.env.PROD ? "/learn-astro" : "/"
+  site: "https://ukesjtu.github.io",
+  base: import.meta.env.PROD ? "/learn-astro" : "/",
+  integrations: [preact()]
 });
