8e1eba9fdbdf5b367b2c7955d43f6f0c9719b83b
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2026-01-22 15:23:32 +0800

    fix: correct site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ukesjtu.github.io",
-  base: import.meta.env.PROD ? "/learn-astro" : "/",
+  site: "https://ukesjtu.github.io/",
+  base: "/learn-astro",
   integrations: [preact()]
 });
