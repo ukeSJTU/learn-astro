daa3575fcf286eed670360c54d27ed4f1c11196d
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2026-01-22 12:59:03 +0800

    feat: add GitHub Actions workflow for deployment and configure site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://ukesjtu.github.io",
+    base: import.meta.env.PROD ? "/learn-astro" : "/"
+});
